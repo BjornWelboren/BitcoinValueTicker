79c284279af740958ad45c5c22964f8b2b7f321c
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-13 14:15:42 +0200

    Added buttons and text

--- app/src/main/java/com/example/bjornwelboren/bitcoinvalueticker/MainActivity.java
+++ app/src/main/java/com/example/bjornwelboren/bitcoinvalueticker/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.bjornwelboren.bitcoinvalueticker;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnExit = findViewById(R.id.btnExit);
+        Button btnGetValue = findViewById(R.id.btnGetValue);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "btnExit has been pressed");
+                finish();
+                System.exit(0);
+            }
+        });
+
+        btnGetValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "btnGetValue has been pressed");
+            }
+        });
+
     }
+
 }
